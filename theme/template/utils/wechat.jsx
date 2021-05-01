ce10ad5eb1af2e555bb1d1637efbe69ed5d5e824
ZhangWei-KUMO <kanseefoil@gmail.com>
2021-05-01 08:55:18 +0800

    add four certificates for webside

--- theme/template/utils/wechat.jsx
+++ theme/template/utils/wechat.jsx
@@ -19,6 +19,15 @@ async function executeSdk(currentUrl) {
     signature: sign.signature,
     jsApiList: ["updateAppMessageShareData", "updateTimelineShareData"]
   });
+  wx.getLocation({
+    type: "wgs84", // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+    success(res) {
+      const { latitude } = res; // 纬度，浮点数，范围为90 ~ -90
+      const { longitude } = res; // 经度，浮点数，范围为180 ~ -180。
+      const { speed } = res; // 速度，以米/每秒计
+      const { accuracy } = res; // 位置精度
+    }
+  });
 }
 
 export default executeSdk;
